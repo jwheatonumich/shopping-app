92dc362c9db47ad5e80dc5b1218f0ce1e50e2ec7
James Heaton <jwheatonumich@gmail.com>
2021-03-28 17:06:49 -0500

    Version 1.0
    Added Readme
    Added file with array of recipe data
    Parse data into a list of recipes
    Create form to let user select recipes
    Added function to add selected recipes into array
    Added function to clear page contents
    Added function to create array of ingredients in selected ingredients
    Added function to sum amount of each ingredient
    Added function to display amount of ingredients on page

---
+++ README.md
@@ -0,0 +1,3 @@
+Purpose: Allow users to select recipies they intend to cook and provide a list of the required ingredients.
+
+Author: James Heaton
---
+++ index.js
@@ -0,0 +1,117 @@
+//Create a list of every recipe
+let recipeList = []
+
+for (i in data){
+    
+    if (!recipeList.includes(data[i].Recipe)){
+        recipeList.push(data[i].Recipe)
+    }
+    
+}
+
+//Create a form that allows user to select recipies
+let controlDiv = document.querySelector("#controlDiv")
+form = document.createElement("form")
+form.setAttribute("id","formDiv")
+controlDiv.appendChild(form)
+
+let formDiv = document.querySelector("#formDiv")
+
+for (i in recipeList){
+    input = document.createElement("input")
+    input.setAttribute("type","checkbox")
+    input.setAttribute("value",recipeList[i])
+
+    label = document.createElement("label")
+    label.textContent = recipeList[i]
+
+    linebreak = document.createElement("br");
+
+    formDiv.appendChild(input)
+    formDiv.appendChild(label)
+    formDiv.appendChild(linebreak)
+}
+
+submitButton = document.createElement("input")
+submitButton.setAttribute("id","submit")
+submitButton.setAttribute("type","button")
+submitButton.setAttribute("value","Submit")
+formDiv.appendChild(submitButton)
+
+//Store selected recipie names in array
+let selectedRecipies = []
+
+let checkboxes = document.querySelectorAll("input[type=checkbox]");
+let submit = document.getElementById("submit");
+
+function getChecked() {
+  let checked = [];
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    let checkbox = checkboxes[i];
+    if (checkbox.checked) {
+            checked.push(checkbox.value);
+        }
+  }
+  return checked;
+}
+
+//Delete contents of a div
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+//Create an array with the total ingredients for the selected recipies
+let ingredients = []; 
+
+function totalIngredients(){
+    for (i in data){
+    
+        if (selectedRecipies.includes(data[i].Recipe)){
+            ingredients.push(data[i])
+        }
+        
+    }
+}
+
+//Create an array with the count of each ingredient in the selected recipies
+let ingredientsCount = [];
+
+function countIngredients(objArr){
+    // first, convert data into a Map with reduce
+    let counts = objArr.reduce((prev, curr) => {
+        let count = prev.get(curr.Ingredient) || 0;
+        prev.set(curr.Ingredient, curr.Quantity + count);
+        return prev;
+    }, new Map());
+  
+  // then, map your counts object back to an array
+  let reducedObjArr = [...counts].map(([Ingredient, Quantity]) => {
+    return {Ingredient, Quantity}
+  })
+  return reducedObjArr
+}
+
+//Display the ingredients on the page
+function displayIngredients(){
+    for (i in ingredientsCount){
+        ingredientPara = document.createElement("p");
+        input.setAttribute("class","ingredientPara");
+        ingredientPara.textContent = ingredientsCount[i].Ingredient + ": " + ingredientsCount[i].Quantity;
+    
+        linebreak = document.createElement("br");
+    
+        controlDiv.appendChild(ingredientPara);
+        controlDiv.appendChild(linebreak);
+    }
+}
+
+//What happens when submit button is clicked
+submit.addEventListener("click", function() {
+  selectedRecipies = getChecked();
+  clearBox("controlDiv");
+  totalIngredients();
+  ingredientsCount = countIngredients(ingredients);
+  displayIngredients()
+});
